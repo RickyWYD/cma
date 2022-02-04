dbaf46e4dd04d652184c362f45c0e704c1800478
youdao wang <47827677+RickyWYD@users.noreply.github.com>
2022-02-04 12:46:28 +0000

    Update cmapss.py

--- utils/cmapss.py
+++ utils/cmapss.py
@@ -52,6 +52,39 @@ def load_data(data_folder):
     # data.columns = cols
     return data
 
+def load_data_test(data_folder):
+    # Read the CSV files
+    fnames = ['test_FD001', 'test_FD002', 'test_FD003', 'test_FD004']
+    cols = ['machine', 'cycle', 'p1', 'p2', 'p3'] + [f's{i}' for i in range(1, 22)]
+    datalist = []
+    nmcn = 0
+    for fstem in fnames:
+        # Read data
+        data = pd.read_csv(f'{data_folder}/{fstem}.txt', sep=' ', header=None)
+        # Drop the last two columns (parsing errors)
+        data.drop(columns=[26, 27], inplace=True)
+        # Replace column names
+        data.columns = cols
+        # Add the data source
+        data['src'] = fstem
+        # Shift the machine numbers
+        data['machine'] += nmcn
+        nmcn += len(data['machine'].unique())
+        # Generate RUL data
+        cnts = data.groupby('machine')[['cycle']].count()
+        cnts.columns = ['ftime']
+        data = data.join(cnts, on='machine')
+        data['rul'] = data['ftime'] - data['cycle']
+        data.drop(columns=['ftime'], inplace=True)
+        # Store in the list
+        datalist.append(data)
+    # Concatenate
+    data = pd.concat(datalist)
+    # Put the 'src' field at the beginning and keep 'rul' at the end
+    data_test = data[['src'] + cols + ['rul']]
+    # data.columns = cols
+    return data_test
+
 
 def plot_dataframe(data, labels=None, vmin=-1.96, vmax=1.96,
         figsize=figsize, autoclose=True, s=4):
